f865bd8e87c8cd8472834a66898aaa6666164425
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-07-25 09:04:31 +0000

    fix design footer

--- pages/component/Footer.js
+++ pages/component/Footer.js
@@ -2,7 +2,7 @@
 
 const Footer=()=>{
     return (
-    <footer className="p-4 mt-8 bg-lime-500 text-gray-50 object-bottom ">
+    <footer className="absolute bottom-0 w-full p-4  mt-8 bg-lime-500 text-gray-50  ">
         &copy;2023
     </footer>
     )
--- pages/component/Header.js
+++ pages/component/Header.js
@@ -2,7 +2,7 @@
 
 const Header =()=>{
     return (
-        <header className="flex justify-start w-full h-1/4 bg-lime-500 ">
+        <header className="flex justify-start p-4 w-full h-1/4 bg-lime-500 ">
         <h1 className="h-1/4">   Cookie Stand Admin  </h1>
       </header>
     )
